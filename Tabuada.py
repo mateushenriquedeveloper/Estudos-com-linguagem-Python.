9e4ba2539410dc77c394f0084af753ee1a81594f
Mateus Henrique Fonseca Xavier da Silva <76543243+mateushenriquefonsecaxavierdasilva@users.noreply.github.com>
2021-07-07 15:46:39 -0300

    2° Estudo com Python.py

---
+++ Tabuada.py
@@ -0,0 +1,17 @@
+valor1 = int(input("Informe um número:"))
+
+tabuada = valor1*0, valor1*1, valor1*2, valor1*3, valor1*4, valor1*5, valor1*6, valor1*7, valor1*8, valor1*9, valor1*10
+
+print("Tabuada do número ({}) = ".format(valor1), tabuada)
+print(" {}x0 = {}".format(valor1, valor1*0))
+print(" {}x1 = {}".format(valor1, valor1*1))
+print(" {}x2 = {}".format(valor1, valor1*2))
+print(" {}x3 = {}".format(valor1, valor1*3))
+print(" {}x4 = {}".format(valor1, valor1*4))
+print(" {}x5 = {}".format(valor1, valor1*5))
+print(" {}x6 = {}".format(valor1, valor1*6))
+print(" {}x7 = {}".format(valor1, valor1*7))
+print(" {}x8 = {}".format(valor1, valor1*8))
+print(" {}x9 = {}".format(valor1, valor1*9))
+print(" {}x10 = {}".format(valor1, valor1*10))
+
